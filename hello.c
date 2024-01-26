0e9d96d79f4b5ac5d6a2105c45e671e694a65d9b
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-01-26 10:59:24 +0530

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+   int number;
+   float decimale_Value;
+   printf("Ente a number:-");
+   scanf("%f", &decimale_Value);
+   printf("%f", decimale_Value);
+}
