70db884d756e1faae83610a1af448a348a1ab6d2
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-01-26 19:28:02 +0530

    Add the if statement

---
+++ condition.c
@@ -0,0 +1,25 @@
+//  profit or loss
+//  cp and sp
+
+#include <stdio.h>
+main()
+{
+    int cp, sp;
+    printf("Enter cost price:");
+    scanf("%d", &cp);
+    printf("Enter  selling price :");
+    scanf("%d", &sp);
+
+    if (cp < sp)
+    {
+        printf("Profit");
+    }
+    else if (cp > sp)
+    {
+        printf("loss");
+    }
+    else
+    {
+        printf("No loss or No profit");
+    }
+}
